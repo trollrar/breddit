d0ab0673a48c59d16775921281103f097a9d8519
fares <troll.rar@gmail.com>
2023-07-22 14:07:14 +0200

    fix memory leak

--- src/app/user/login/login-form/login-form.component.ts
+++ src/app/user/login/login-form/login-form.component.ts
@@ -1,11 +1,12 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {UserService} from '../../user.service';
+import {take} from 'rxjs/operators';
 
 @Component({
-  selector: 'bread-login-form',
-  templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+    selector: 'bread-login-form',
+    templateUrl: './login-form.component.html',
+    styleUrls: ['./login-form.component.scss']
 })
 export class LoginFormComponent implements OnInit {
     public form: FormGroup;
@@ -30,6 +31,7 @@ export class LoginFormComponent implements OnInit {
         this.submitted = true;
 
         this.userService.fetchAuthToken(formData.username, formData.password)
+            .pipe(take(1))
             .subscribe(
                 () => {
                     this.loggedIn.emit(true);
